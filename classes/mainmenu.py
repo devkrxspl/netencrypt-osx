3a46bab769eaf71354e15067308d215d738515f0
Daniel Long <devkrxspl@gmail.com>
2021-09-25 14:58:30 -0400

    Changed main menu y pos from scale to offset

--- classes/mainmenu.py
+++ classes/mainmenu.py
@@ -21,9 +21,9 @@ class MainMenu(npyscreen.FormBaseNew):
 
         menu_title = "Welcome to NetEncrypt";
 
-        self.add(npyscreen.TitleFixedText, name = menu_title, relx = (x_max - 23) // 2, rely = y_max // 2 - 5);
-        self.add(npyscreen.ButtonPress, name="[ Login ]", relx = (x_max - 14) // 2, rely = y_max // 2 - 3, when_pressed_function=self.login);
-        self.add(npyscreen.ButtonPress, name="[ Exit  ]", relx = (x_max - 14) // 2, rely = y_max // 2 - 1, when_pressed_function=self.exit);
+        self.add(npyscreen.TitleFixedText, name = menu_title, relx = (x_max - 23) // 2, rely = 7);
+        self.add(npyscreen.ButtonPress, name="[ Connect ]", relx = (x_max - 14) // 2, rely = 9, when_pressed_function=self.login);
+        self.add(npyscreen.ButtonPress, name="[  Exit   ]", relx = (x_max - 14) // 2, rely = 11, when_pressed_function=self.exit);
         connection_status = self.add(npyscreen.FixedText, value="Disconnected", relx=2, rely=1);
 
         # Connected
@@ -41,7 +41,7 @@ class MainMenu(npyscreen.FormBaseNew):
     def login(self):
         if (not client.sio.connected):
             y_max, x_max = curses.initscr().getmaxyx();
-            notify.notify_confirm("  Failed to establish a connection to the server.", "", form_color="WHITE_BLACK", editw=1, width=x_max - 10, height=y_max//2, relx = 5, rely=5);
+            notify.notify_confirm("  Failed to establish a connection to the server.", "", form_color="WHITE_BLACK", editw=1, width=x_max - 10, height=y_max//2, relx = 5, rely=3);
 
     def exit(self):
 
